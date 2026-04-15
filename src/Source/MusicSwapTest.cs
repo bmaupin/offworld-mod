ab0ed4c1cc38c42b4bc2f2636050c0836361bf1b
bmaupin <bmaupin@users.noreply.github.com>
2026-04-15 13:21:05 -0400

    feat: Test overriding track 1 with random track

--- src/Source/MusicSwapTest.cs
+++ src/Source/MusicSwapTest.cs
@@ -7,18 +7,31 @@ using UnityEngine;
 public class MusicSwapTest : ModEntryPointAdapter
 {
     private static readonly string[] Songs = new string[] { "A", "B", "C", "D", "E", "F" };
+    private static readonly System.Random Random = new System.Random();
+    private static int selectedTrackNumber = -1;
+
+    public override void Initialize()
+    {
+        if (selectedTrackNumber < 2)
+        {
+            // Pick once per Offworld launch. Reused for all matches in this session.
+            selectedTrackNumber = Random.Next(2, 16);
+        }
+
+        Debug.Log("[MusicSwapTest] Selected replacement track index: " + selectedTrackNumber);
+    }
 
     public override void OnGameClientReady()
     {
-        ApplyTrackOneToTrackTwelveSwap("OnGameClientReady");
+        ApplyTrackOneRandomSwap("OnGameClientReady");
     }
 
     public override void OnPostLoadGameClient()
     {
-        ApplyTrackOneToTrackTwelveSwap("OnPostLoadGameClient");
+        ApplyTrackOneRandomSwap("OnPostLoadGameClient");
     }
 
-    private static void ApplyTrackOneToTrackTwelveSwap(string source)
+    private static void ApplyTrackOneRandomSwap(string source)
     {
         if (Globals.Infos == null)
         {
@@ -26,38 +39,44 @@ public class MusicSwapTest : ModEntryPointAdapter
             return;
         }
 
+        if (selectedTrackNumber < 2)
+        {
+            selectedTrackNumber = Random.Next(2, 16);
+            Debug.Log("[MusicSwapTest] Late-selected replacement track index: " + selectedTrackNumber);
+        }
+
         int swaps = 0;
 
         foreach (string song in Songs)
         {
             string node1Type = "AUDIO_GAME_MUSIC_NODE_" + song + "1";
-            string node12Type = "AUDIO_GAME_MUSIC_NODE_" + song + "12";
+            string replacementType = "AUDIO_GAME_MUSIC_NODE_" + song + selectedTrackNumber;
 
             AudioTypeT node1 = Globals.Infos.getType<AudioTypeT>(node1Type, false);
-            AudioTypeT node12 = Globals.Infos.getType<AudioTypeT>(node12Type, false);
+            AudioTypeT replacement = Globals.Infos.getType<AudioTypeT>(replacementType, false);
 
-            if (node1 == AudioTypeT.NONE || node12 == AudioTypeT.NONE)
+            if (node1 == AudioTypeT.NONE || replacement == AudioTypeT.NONE)
             {
-                Debug.Log("[MusicSwapTest] Missing type: " + node1Type + " or " + node12Type);
+                Debug.Log("[MusicSwapTest] Missing type: " + node1Type + " or " + replacementType);
                 continue;
             }
 
             InfoAudio info1 = Globals.Infos.audio(node1);
-            InfoAudio info12 = Globals.Infos.audio(node12);
+            InfoAudio replacementInfo = Globals.Infos.audio(replacement);
 
-            if (info1 == null || info12 == null)
+            if (info1 == null || replacementInfo == null)
             {
-                Debug.Log("[MusicSwapTest] Missing audio info: " + node1Type + " or " + node12Type);
+                Debug.Log("[MusicSwapTest] Missing audio info: " + node1Type + " or " + replacementType);
                 continue;
             }
 
-            info1.mzAsset = info12.mzAsset;
-            info1.mzClassicAsset = info12.mzClassicAsset;
+            info1.mzAsset = replacementInfo.mzAsset;
+            info1.mzClassicAsset = replacementInfo.mzClassicAsset;
             swaps++;
 
-            Debug.Log("[MusicSwapTest] " + node1Type + " now uses asset from " + node12Type + ": " + info1.mzAsset);
+            Debug.Log("[MusicSwapTest] " + node1Type + " now uses asset from " + replacementType + ": " + info1.mzAsset);
         }
 
-        Debug.Log("[MusicSwapTest] Applied swaps from " + source + ": " + swaps + " song(s)");
+        Debug.Log("[MusicSwapTest] Applied random swaps from " + source + ": " + swaps + " song(s)");
     }
 }
